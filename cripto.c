c8370e8ce0e107fe3d4c439b12e71c4c2a1b7343
aluno <aluno@Micro01>
2018-07-18 11:09:06 -0300

    novo criptoincial

--- cripto.c
+++ cripto.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+#define MAX 100;
 
+int main( int argc, char *argv[ ]){	
+	int i,x,y;
+	for(i=0;i< argc;i++){
+		x = strcmp( argv[i],"-c");
+		y = strcmp( argv[i],"-d")
+		if( x == 0 ){
+			criptografar();
+			break;
+		}
+		if( y == 0){
+			descriptografar();
+			break;
+		}
+	}
+	return 0;
+}
+
+void criptografar(){
+	int vetint[MAX]={'\0'};	
+	while(fgets(vetint,MAX,stdin) != NULL){
+		
+	}	
 }
